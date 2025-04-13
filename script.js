f733ef5073e7b48ff3794ae96719a73a502047b7
Stijn Laurent <46028254+laurentstijn@users.noreply.github.com>
2025-04-13 12:17:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,6 @@ let dragging = false;
 let startX, startY;
 let editBootId = null;  // Dit wordt gebruikt om de boot te bewerken
 
-// Open de popup voor het toevoegen van een nieuwe boot
-function openPopupNew() {
-  document.getElementById('popupTitel').textContent = "Nieuwe boot toevoegen";
-  document.getElementById('bootNaam').value = "";
-  document.getElementById('bootLengte').value = "12"; // Standaard lengte
-  document.getElementById('bootBreedte').value = "4"; // Standaard breedte
-  document.getElementById('bootEigenaar').value = "";
-  editBootId = null;  // Geen id voor een nieuwe boot
-  document.getElementById('popup').style.display = 'block'; // Toon de popup
-}
 
 
 // Firebase configuratie (gebruik je eigen configuratie)
@@ -358,6 +348,17 @@ function bevestigBoot() {
 // Functie voor annuleren
 function annuleerBoot() {
   document.getElementById('popup').style.display = 'none'; // Sluit de popup zonder wijzigingen
+}
+
+// Open de popup voor het toevoegen van een nieuwe boot
+function openPopupNew() {
+  document.getElementById('popupTitel').textContent = "Nieuwe boot toevoegen";
+  document.getElementById('bootNaam').value = "";
+  document.getElementById('bootLengte').value = "12"; // Standaard lengte
+  document.getElementById('bootBreedte').value = "4"; // Standaard breedte
+  document.getElementById('bootEigenaar').value = "";
+  editBootId = null;  // Geen id voor een nieuwe boot
+  document.getElementById('popup').style.display = 'block'; // Toon de popup
 }
 
 // Voeg de eventlisteners toe voor de knoppen in de popup
